49cfa95dcb0ef9d113744016607cf596b3c59b50
Simeon Slashchinin <sem63sam@yandex.ru>
2024-05-09 21:06:49 +0400

    add README.md

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,22 +1,21 @@
+
 import exceptions.WithdrawalAmountException;
 import model.Account;
 import service.AccountService;
 
 public class Program {
     public static void main(String[] args) {
-
         AccountService accountService = new AccountService();
 
         Account account_001 = accountService.createAccount("001");
         Account account_002 = accountService.createAccount("002");
 
         try {
-            accountService.addFundsToTheAccount(account_002, -5d);
             accountService.addFundsToTheAccount(account_001, 10d);
             accountService.withdrawFundsFromTheAccount(account_001, 6d);
             accountService.withdrawFundsFromTheAccount(account_001, 10d);
-        } catch (WithdrawalAmountException e) {
-            e.printStackTrace();
+        } catch (IllegalArgumentException | WithdrawalAmountException e) {
+            System.err.println("An error occurred: " + e.getMessage());
         }
     }
 }
